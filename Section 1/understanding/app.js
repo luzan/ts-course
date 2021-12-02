d0b3422867a7001e4056f4c06f7c0963fa1a82b3
Luzan Baral <luzanb@techkraftinc.com>
2021-12-02 14:30:00 +0545

    Initialize project

---
+++ Section 1/understanding/app.js
@@ -0,0 +1,5 @@
+var person = {
+    name: "Luzan",
+    age: 28
+};
+console.log(person.name);
---
+++ Section 1/understanding/app.ts
@@ -0,0 +1,10 @@
+// const person: {
+//   name: string;
+//   age: number;
+// }
+const person = {
+  name: "Luzan",
+  age: 28,
+};
+
+console.log(person.name);
