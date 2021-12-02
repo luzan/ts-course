9fdc95350c7db814f8bdc1269c311de68e962b96
Luzan Baral <luzanb@techkraftinc.com>
2021-12-02 14:39:02 +0545

    array

--- Section 1/understanding/app.ts
+++ Section 1/understanding/app.ts
@@ -5,6 +5,14 @@
 const person = {
   name: "Luzan",
   age: 28,
+  hobbies: ["Sports", "Cooking"],
 };
 
+let favoriteActivities: string[];
+favoriteActivities = ["Sports"];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
